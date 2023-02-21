e5ba4b20d60d8ff50d99a77f1c66bcd47c9ad4f2
Jenna Bonke <jbonke@hotmail.nl>
2023-02-21 14:34:28 +0100

    Add temp code

--- scripts/wordreplace.js
+++ scripts/wordreplace.js
@@ -5,10 +5,18 @@ let words = [
     {s: /girl/g, r: "catgirl"}
 ];
 
-let elems = document.querySelectorAll("body *:not(script):not(noscript):not(style)")
+let elems = document.querySelectorAll("body *:not(script):not(noscript):not(style)");
+let b = document.body;
 words.forEach(x => {
     try {
         b.innerHTML = b.innerHTML.replace(x.s, x.r);
+        // TODO: Figure out how to regex match for a pair of <script> tags
+        // https://stackoverflow.com/questions/406230/regular-expression-to-match-a-line-that-doesnt-contain-a-word
+
+        
+        // elems.forEach(y => {
+        //     y.innerHTML = y.innerHTML.replace(x.s, x.r);
+        // });
     }
     catch (e) {
         console.warn(`[WordReplace] query (${x.s}) or replacement (${x.r}) is not a valid value. \n\n Error: ${e}`);
